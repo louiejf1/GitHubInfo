df2a2d77b0b7cfab555be81b66fc37d421a113c6
Louie <louiejf1@gmail.com>
2020-07-14 18:33:23 -0700

    first commit

---
+++ precache-manifest.702b9337e629e5c6b4cb0ee75f7312b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ccd64ff4c70292de371b78a1657f6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d078793291cc2ab5260",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c747c358487a6bdb28a",
+    "url": "/static/js/2.b7aaffb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7aaffb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d078793291cc2ab5260",
+    "url": "/static/js/main.cc495093.chunk.js"
+  },
+  {
+    "revision": "6215561f3a54175e79d5",
+    "url": "/static/js/runtime-main.f78506f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
